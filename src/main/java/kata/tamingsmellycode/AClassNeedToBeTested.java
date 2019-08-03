f45363edb7611ec6f99981d0de7e2169f28a27a8
benwu <wubinben@icloud.com>
2019-08-03 13:29:45 +0800

    update kata numbers

--- src/main/java/kata/tamingsmellycode/AClassNeedToBeTested.java
+++ src/main/java/kata/tamingsmellycode/AClassNeedToBeTested.java
@@ -5,48 +5,47 @@ public class AClassNeedToBeTested {
 
     public AClassNeedToBeTested() {
         this.memberVariable = new NewADependedOnComponentWhichToBeMocked();
-        // 3. Parameterize Constructor
-        // 9. Extract & Override Factory Method
+        // k3. Parameterize Constructor
+        // k5. Extract & Override Factory Method
     }
 
-    public AClassNeedToBeTested(DependedOnComponentNeedToBeMocked dependedOnComponentNeedToBeMocked) {
-        // 1. Extract Interface
+    public AClassNeedToBeTested(ParameterToBeMOcked parameterToBeMOcked) {
+        // k1. Extract Interface
     }
 
-    public void aMethod(ClassWithTooManyMethodsToMock classWithTooManyMethodsToMock) {
-        // 2. Adapt Parameter
+    public void aMethod(HttpServletRequestWhichIsHardToMock httpServletRequestWhichIsHardToMock) {
+        // k2. Adapt Parameter
     }
 
     public void anotherMethod() {
         NewADependedOnComponentWhichToBeMocked aVariable =
                 new NewADependedOnComponentWhichToBeMocked();
-        // 4. Parameterize Method
-        // 8. Subclass & Override Method
+        // k4. Parameterize Method
+        // k5. Extract & Override Factory Method
     }
 
     public void theThirdMethod() {
         AService.aStaticMethodWhichToBeMocked();
-        // 5. Introduce Instance Delegator
+        // k6. Introduce Instance Delegator
     }
 
     public void theFourthMethod() {
         String aVariable = AnotherService.aStaticMethodWhichToBeMocked();
-        // 10. Extract & Override Call
-        // 11. Replace Global Reference with Getter
+        // k7. Extract & Override Call
     }
 
-    public void aLargeMethodUsingManyInstanceDataAndMethodsNotToBeTested() {
-        // 6. Break out Method Object
+    public void aLargeMethodWithManyInstanceDataAndMethodsNotToBeTested() {
+        // k9. Break out Method Object
     }
 
     public void aMethodUsedSingletonWhichToBeMocked() {
         Singleton singleton = Singleton.getInstance();
-        // 7. Introduce Static Setter
+        // k8. Introduce Static Setter
     }
 
     public void aMethodUsedAnInstanceCreatedFromAGlobalFactoryWhichToBeMocked() {
         Product product = GlobalFactory.makeProduct();
-        // 7. Introduce Static Setter
+        // k8. Introduce Static Setter
     }
 }
 
--- src/main/java/kata/tamingsmellycode/DependedOnComponentNeedToBeMocked.java
+++
@@ -1,4 +0,0 @@
-package kata.tamingsmellycode;
-
-public class DependedOnComponentNeedToBeMocked {
-}
